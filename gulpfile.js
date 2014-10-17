2b1eff1da1924720463e15aeac9ff9b67da978a8
Horiuchi_H <horiuchi.g@gmail.com>
2014-10-17 18:24:49 +0900

    add unit test settings

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,30 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var del = require('del');
+// for power-assert
+require('intelli-espower-loader');
 
 
 var paths = {
   tsc: {
     src: 'lib/**/*.ts',
     dest: 'lib/',
-    clean: 'lib/**/*.js*'
+    test: {
+      src: 'test/**/*.ts',
+      dest: 'test/'
+    },
+    clean: ['lib/**/*.js*', 'test/**/*.js*']
+  },
+  mocha: {
+    src: ['test/**/*.js']
   }
 };
 var tsProject = plugins.typescript.createProject({
+  target: 'ES5',
+  module: 'commonjs',
+  noImplicitAny: true,
+});
+var tsTestProject = plugins.typescript.createProject({
   target: 'ES5',
   module: 'commonjs',
   noImplicitAny: true,
@@ -27,6 +41,14 @@ gulp.task('compile', function() {
     .pipe(plugins.sourcemaps.write())
     .pipe(gulp.dest(paths.tsc.dest));
 });
+gulp.task('compile-test', function() {
+  var tsResult = gulp.src([paths.tsc.test.src])
+                  .pipe(plugins.sourcemaps.init())
+                  .pipe(plugins.typescript(tsTestProject));
+  tsResult.js
+    .pipe(plugins.sourcemaps.write())
+    .pipe(gulp.dest(paths.tsc.test.dest));
+});
 
 gulp.task('tsd', function(cb) {
   plugins.tsd({
@@ -36,17 +58,19 @@ gulp.task('tsd', function(cb) {
 });
 
 
-gulp.task('test', function() {
+gulp.task('test', ['compile', 'compile-test'], function() {
+  gulp.src(paths.mocha.src, {read: false})
+    .pipe(plugins.mocha({reporter: 'tap'}));
 });
 
 
 gulp.task('clean', function(cb) {
-  del([paths.tsc.clean], cb);
-
+  del(paths.tsc.clean, cb);
 });
 
 gulp.task('watch', function() {
-  gulp.watch([paths.tsc.src], ['compile']);
+  gulp.watch([paths.tsc.src], ['test']);
+  gulp.watch([paths.tsc.test.src], ['test']);
 });
 
 
