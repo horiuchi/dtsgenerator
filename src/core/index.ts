62f594157159e9118bb2c1e589146da4cabf5ce7
Hiroki Horiuchi <horiuchi.g@gmail.com>
2020-03-06 18:02:34 +0900

    feat: remove schemaConvertor and writeProcessor

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,14 +1,12 @@
 import DtsGenerator from './dtsGenerator';
 import { parseSchema } from './jsonSchema';
 import ReferenceResolver from './referenceResolver';
-import SchemaConvertor from './schemaConvertor';
 import { TypeNameConvertor } from './typeNameConvertor';
-import WriteProcessor, { WriteProcessorOptions } from './writeProcessor';
 
 export { default as SchemaId } from './schemaId';
 export { DefaultTypeNameConvertor } from './typeNameConvertor';
 
-export interface Options extends Partial<WriteProcessorOptions> {
+export interface Options {
     contents?: any[];
     inputUrls?: string[];
     typeNameConvertor?: TypeNameConvertor;
@@ -16,9 +14,7 @@ export interface Options extends Partial<WriteProcessorOptions> {
 }
 
 export default async function dtsGenerator(options: Options): Promise<string> {
-    const processor = new WriteProcessor(options);
     const resolver = new ReferenceResolver();
-    const convertor = new SchemaConvertor(processor, options.typeNameConvertor, options.namespaceName);
 
     if (options.contents != null) {
         options.contents
@@ -29,6 +25,6 @@ export default async function dtsGenerator(options: Options): Promise<string> {
         await Promise.all(options.inputUrls.map((url) => resolver.registerRemoteSchema(url)));
     }
 
-    const generator = new DtsGenerator(resolver, convertor);
+    const generator = new DtsGenerator(resolver);
     return await generator.generate();
 }
