7ceadef55fd680579d836a635861116f71726540
Hiroki Horiuchi <horiuchi.g@gmail.com>
2020-03-12 18:29:11 +0900

    fix: plugin implementation

--- src/core/config.ts
+++ src/core/config.ts
@@ -15,15 +15,17 @@ const defaultConfig: Config = {
     outputAST: false,
     target: ts.ScriptTarget.Latest,
     typeNameConvertor: DefaultTypeNameConvertor,
-    plugins: {
-        'replace-namespace': {},
-    },
+    plugins: {},
 };
 
-let config: Config = defaultConfig;
+let config: Config = Object.assign({}, defaultConfig);
 
 export function setConfig(input: Partial<Config>): void {
     config = Object.assign(config, input);
+}
+
+export function clearToDefault(): void {
+    setConfig(defaultConfig);
 }
 
 export default config;
