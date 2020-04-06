b35d178e4ee4ac87165c39cc22789585b661c39e
Brian KAVANAGH <55122447+Brian-Kavanagh@users.noreply.github.com>
2020-04-06 13:53:52 +1000

    Patch to handle nested schema 'allOf' keywords.
    Currently, dtsGenerator will 'ignore' nested 'allOf' keywords.
    
    i.e. The Schema:
    
        {
            $id: '/test/nested/allOf',
            $schema: 'http://json-schema.org/draft-07/schema#',
            type: 'object',
            allOf: [ {
                    type: 'object',
                    properties: { a: { type: 'string' } }
                }, {
                    type: 'object',
                    allOf: [
                        {
                            type: 'object',
                            properties: { b: { type: 'string' } }
                        },
                        {
                            type: 'object',
                            properties: { c: { type: 'string' } }
                        }
                    ]
                } ],
            required: ['a', 'b', 'c']
        }
    
    Will only generate:
    
        declare namespace Test {
            namespace Nested {
                export interface AllOf {
                    a: string;
                    // Missing 'b', 'c' properties.
                }
            }
        }
    
    This commit adds changes to recursively scan for nested 'allOf' keywords within
    the schema, and generate the properties associated with the nested elements.

---
+++ test/nested_allof_test.ts
@@ -0,0 +1,134 @@
+import * as assert from 'assert';
+import dtsGenerator from '../src/core';
+
+describe('nested \'allOf\' test', () => {
+    it ('single \'allOf\' nesting schema', async () => {
+        const schema: JsonSchemaOrg.Draft07.Schema = {
+            $id: '/test/nested/allOf',
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            allOf: [
+                {
+                    type: 'object',
+                    properties: {
+                        a: { type: 'string' }
+                    }
+                },
+                {
+                    type: 'object',
+                    allOf: [
+                        {
+                            type: 'object',
+                            properties: {
+                                b: { type: 'string' }
+                            }
+                        },
+                        {
+                            type: 'object',
+                            properties: {
+                                c: { type: 'string' }
+                            }
+                        },
+                    ]
+                }
+            ],
+            required: ['a', 'b', 'c'],
+        };
+        const result = await dtsGenerator({contents: [schema]});
+
+        const expected = `declare namespace Test {
+    namespace Nested {
+        export interface AllOf {
+            a: string;
+            b: string;
+            c: string;
+        }
+    }
+}
+`;
+        assert.equal(result, expected, 'Nested \'allOf\' definitions should result in all properties being included in the output interface.');
+    });
+    it ('multiple \'allOf\' nestings schema', async () => {
+        const schema: JsonSchemaOrg.Draft07.Schema = {
+            $id: '/test/nested/allOf',
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            allOf: [
+                {
+                    type: 'object',
+                    properties: {
+                        a: { type: 'string' }
+                    }
+                },
+                {
+                    type: 'object',
+                    allOf: [
+                        {
+                            type: 'object',
+                            properties: {
+                                b: { type: 'string' }
+                            }
+                        },
+                        {
+                            type: 'object',
+                            allOf: [
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        c: { type: 'string' }
+                                    }
+                                },
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        d: { type: 'string' }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            type: 'object',
+                            allOf: [
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        c: { type: 'string' }
+                                    }
+                                },
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        e: { type: 'string' }
+                                    }
+                                },
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        f: { type: 'string' }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ],
+            required: ['a', 'b', 'c', 'd', 'e', 'f'],
+        };
+        const result = await dtsGenerator({contents: [schema]});
+
+        const expected = `declare namespace Test {
+    namespace Nested {
+        export interface AllOf {
+            a: string;
+            b: string;
+            c: string;
+            d: string;
+            e: string;
+            f: string;
+        }
+    }
+}
+`;
+        assert.equal(result, expected, 'Nested \'allOf\' definitions should result in all properties being included in the output interface.');
+    });
+});
