5a941a7b6a6bd79bb344c23f5e6d87cd7de7b562
Hiroki Horiuchi <horiuchi.g@gmail.com>
2021-01-05 17:14:14 +0900

    feat: add void type support

---
+++ test/snapshots/openapi-v3/void_response/_expected.d.ts
@@ -0,0 +1,12 @@
+declare namespace Paths {
+    namespace Create {
+        namespace Post {
+            export interface RequestBody {
+                id?: string;
+            }
+            namespace Responses {
+                export type $204 = void;
+            }
+        }
+    }
+}
