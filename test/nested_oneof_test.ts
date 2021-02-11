84e6e52cf0695dfbacbfa7e4a8495942919983ba
medfreeman <mlahlou@protonmail.ch>
2021-02-11 13:00:25 +0100

    test: add additional nested `oneOf` tests

--- test/nested_oneof_test.ts
+++ test/nested_oneof_test.ts
@@ -54,4 +54,156 @@ describe("nested 'oneOf' test", () => {
             "Nested 'oneOf' definitions should result in all properties being included in the output interface."
         );
     });
+
+    it("multiple 'oneOf' nestings schema", async () => {
+        const schema: JsonSchemaDraft07.Schema = {
+            $id: '/test/nested/oneOf',
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            oneOf: [
+                {
+                    type: 'object',
+                    properties: {
+                        a: { type: 'string' },
+                    },
+                },
+                {
+                    type: 'object',
+                    oneOf: [
+                        {
+                            type: 'object',
+                            properties: {
+                                b: { type: 'string' },
+                            },
+                        },
+                        {
+                            type: 'object',
+                            oneOf: [
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        c: { type: 'string' },
+                                    },
+                                },
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        d: { type: 'string' },
+                                    },
+                                },
+                            ],
+                        },
+                        {
+                            type: 'object',
+                            oneOf: [
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        c: { type: 'string' },
+                                    },
+                                },
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        e: { type: 'string' },
+                                    },
+                                },
+                                {
+                                    type: 'object',
+                                    properties: {
+                                        f: { type: 'string' },
+                                    },
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        };
+        const result = await dtsGenerator({ contents: [parseSchema(schema)] });
+
+        const expected = `declare namespace Test {
+    namespace Nested {
+        export type OneOf = {
+            a?: string;
+        } | ({
+            b?: string;
+        } | ({
+            c?: string;
+        } | {
+            d?: string;
+        }) | ({
+            c?: string;
+        } | {
+            e?: string;
+        } | {
+            f?: string;
+        }));
+    }
+}
+`;
+        assert.strictEqual(
+            result,
+            expected,
+            "Nested 'oneOf' definitions should result in all properties being included in the output interface."
+        );
+    });
+
+    it("refed 'oneOf' nested schema", async () => {
+        const schema: JsonSchemaDraft07.Schema = {
+            $id: '/test/nested/oneOf',
+            $schema: 'http://json-schema.org/draft-07/schema#',
+            type: 'object',
+            definitions: {
+                A: {
+                    properties: {
+                        s: {
+                            oneOf: [
+                                {
+                                    type: 'string',
+                                },
+                            ],
+                        },
+                    },
+                },
+                B: {
+                    properties: {
+                        ref: {
+                            oneOf: [
+                                {
+                                    $ref: '#/definitions/A',
+                                },
+                            ],
+                        },
+                    },
+                },
+            },
+            properties: {
+                bref: {
+                    $ref: '#/definitions/B',
+                },
+            },
+        };
+        const result = await dtsGenerator({ contents: [parseSchema(schema)] });
+
+        const expected = `declare namespace Test {
+    namespace Nested {
+        export interface OneOf {
+            bref?: OneOf.Definitions.B;
+        }
+        namespace OneOf {
+            namespace Definitions {
+                export interface A {
+                    s?: string;
+                }
+                export interface B {
+                    ref?: A;
+                }
+            }
+        }
+    }
+}
+`;
+        assert.strictEqual(result, expected);
+    });
 });
